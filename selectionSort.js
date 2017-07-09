78065a73231d489c2e967f5db649700a2e2b7cdc
Drewclam <andrew.c.lam94@gmail.com>
2017-07-09 00:31:08 -0400

    Add pseudocode for selectionsort

---
+++ selectionSort.js
@@ -0,0 +1,12 @@
+// constant space in place: select the largest element and swap at the wall
+var arr = [3,6,2,5,8,1,9,7,4];
+
+function selectionSort(arr) {
+  // linear space: select the smallest element in the array and push into a new array
+  // iterate over arr to i
+    // iterate over arr to j
+      // if arr at j is smaller than smallest
+        // set smallest to arr at j
+    // push smallest to sorted array
+  // return sorted array
+}
